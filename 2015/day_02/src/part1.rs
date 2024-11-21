46781efe6cb1aeeb46c31e048f55d095d622b765
Eric Jolibois <em.jolibois@gmail.com>
2024-11-21 13:37:27 +0100

    chore: clippy on 2015 day 02

--- 2015/day_02/src/part1.rs
+++ 2015/day_02/src/part1.rs
@@ -8,7 +8,7 @@ fn wrapping_paper_for_line(line: &str) -> usize {
         .map(|dim| dim.parse::<usize>().expect("dimension should be positive"))
         .collect();
     assert_eq!(dims.len(), 3);
-    let areas = vec![dims[0] * dims[1], dims[1] * dims[2], dims[2] * dims[0]];
+    let areas = [dims[0] * dims[1], dims[1] * dims[2], dims[2] * dims[0]];
     2 * areas.iter().sum::<usize>() + areas.iter().min().expect("dims should not be empty")
 }
 
